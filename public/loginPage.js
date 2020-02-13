0af2e1fdff378119891a42760b6c68267132efe3
kiselevgleb <48054067+kiselevgleb@users.noreply.github.com>
2020-02-13 16:27:40 +0300

    Update loginPage.js

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,5 +1,21 @@
 'use strict';
 const userForm1 = new UserForm();
 
-userForm1.loginFormCallback = data=>ApiConnector.login(data);
-userForm1.registerFormCallback = data=>ApiConnector.register(data);
+userForm1.loginFormCallback = data=>ApiConnector.login(data,(err, data1)=>{
+    if (err) {
+        location.reload();
+            console.log(`OK`);
+            
+    } else {
+        console.error('Error reg');}
+    });
+
+
+
+userForm1.registerFormCallback = data=>ApiConnector.register(data,(err, data1)=>{
+    if (!err) {
+            console.error('Error reg');
+    } else {
+            location.reload();
+            console.log(`OK`);}
+    });
