f0b9b5c4fe002753a0d2b9f019be3e39edf63b60
kiselevgleb <48054067+kiselevgleb@users.noreply.github.com>
2020-02-18 15:04:08 +0300

    Update homePage.js

---
+++ public/homePage.js
@@ -0,0 +1,101 @@
+'use strict';
+const logoutButton = new LogoutButton();
+const ratesBoard = new RatesBoard();
+const favoritesWidget = new FavoritesWidget();
+const moneyManager = new MoneyManager();
+
+moneyManager.sendMoneyCallback = data => 
+    ApiConnector.transferMoney(data, response => {
+        if (response.success) {
+            ProfileWidget.showProfile(response.data);
+            favoritesWidget.setMessage(!response.success, `OK`);
+        } else {
+            favoritesWidget.setMessage(response.success, `Error`);
+        }
+    });
+
+moneyManager.conversionMoneyCallback = data => 
+    ApiConnector.convertMoney(data, response => {
+        if (response.success) {
+            ProfileWidget.showProfile(response.data);
+            favoritesWidget.setMessage(!response.success, `OK`);
+        } else {
+            favoritesWidget.setMessage(response.success, `Error`);
+        }
+    });
+
+moneyManager.addMoneyCallback = data => 
+    ApiConnector.addMoney(data, response => {
+        if (response.success) {
+            ProfileWidget.showProfile(response.data);
+            favoritesWidget.setMessage(!response.success, `OK`);
+        } else {
+            favoritesWidget.setMessage(response.success, `Error`);
+        }
+    });
+
+favoritesWidget.removeUserCallback = data => 
+    ApiConnector.removeUserFromFavorites(data, response => {
+        if (response.success) {
+            favoritesWidget.clearTable();
+            favoritesWidget.fillTable(response.data);
+            moneyManager.updateUsersList(response.data);
+            favoritesWidget.setMessage(!response.success, `OK`);
+        } else {
+            favoritesWidget.setMessage(response.success, `Error`);
+        }
+    });
+
+favoritesWidget.addUserCallback  = data => 
+    ApiConnector.addUserToFavorites(data, response => {
+        if (response.success) {
+            favoritesWidget.clearTable();
+            favoritesWidget.fillTable(response.data);
+            moneyManager.updateUsersList(response.data);
+            favoritesWidget.setMessage(!response.success, `OK`);
+        } else {
+            favoritesWidget.setMessage(response.success, `Error`);
+        }
+    });
+
+ApiConnector.getFavorites(response => {
+        if (response.success) {
+            favoritesWidget.clearTable();
+            favoritesWidget.fillTable(response.data);
+            moneyManager.updateUsersList(response.data);
+            console.log(`OK`);
+        } else {
+            console.log(`Error`);
+        }
+    });
+logoutButton.action = function () {
+    ApiConnector.logout(response => {
+        if (response.success) {
+            location.reload();
+            console.log(`OK`);
+        } else {
+            console.log(`Error`);
+        }
+    })
+};
+
+ApiConnector.current(response => {
+    if (response.success) {
+        ProfileWidget.showProfile(response.data);
+        console.log(`OK`);
+    } else {
+        console.log(`Error`);
+    }
+});
+let rBoard = function () { 
+    ApiConnector.getStocks(response => {
+        if (response.success) {
+            ratesBoard.clearTable();
+            ratesBoard.fillTable(response.data);
+            console.log(`OK`);
+        } else {
+            console.log(`Error`);
+        }
+    });}
+rBoard();
+setInterval(rBoard, 60000);
